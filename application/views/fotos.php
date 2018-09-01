82cad4765c1011af8b508e3a888b1e4cad03de1d
Matheus Damian Pereira <matheusdamian70@gmail.com>
2018-09-01 15:59:37 -0300

    alteração de design

--- application/views/fotos.php
+++ application/views/fotos.php
@@ -1,6 +1,6 @@
 <script src="../assets/library/jquery.min.js"></script>
 <script src="../assets/library/bootstrap.min.js"></script>
-<link rel="stylesheet" href="../assets/library/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url('assets/library/bootstrap.min.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/fotos/css/style.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/fotos/css/modal_fotos.css')?>">
 <article>
@@ -8,9 +8,9 @@
         <p class="titulo">FOTOS</p>
         <div class="container">
             <div class="col-sm-12">
-                <?php for($i = 1 ; $i <= 6; $i++):?>
+                <?php for($i = 1 ; $i <= 9; $i++):?>
                     <div class="fotos col-sm-4 text-center">
-                        <img class="myImg" src="<?php echo base_url('/assets/fotos/css/'). $i .'.jpg'?>">
+                        <img class="myImg img-thumbnail" src="<?php echo base_url('/assets/fotos/css/'). $i .'.jpg'?>">
                         <p>Lorem Ipsum é simplesmente</p>
                     </div>
                 <?php endfor ?>
@@ -19,6 +19,7 @@
     </div>
 
 <div class='modal_fotos'>
+    <span class="close">&times;</span>
     <div class='box_image'>
         <img src="" alt="" class='img'>
     </div>
@@ -32,14 +33,16 @@ $(document).ready(function(){
         $('.modal_fotos').fadeIn();
         $('.img').attr('src', $(this).attr('src'));
     })
-
     $('.modal_fotos').click(function() {
         $(this).fadeOut();
         
     })
+    $('.close').click(function() {
+        $('.modal_fotos').fadeOut();
+        
+    })
 
 });
-    
 
 </script>
 </article>
