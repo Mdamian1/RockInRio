87b8b7890b928f5c8c3ca814ec1318775411840a
Dionathan de Córdova <dionathan_bass@hotmail.com>
2018-10-16 21:19:53 -0300

    alteração pagina musicas

--- application/views/musica.php
+++ application/views/musica.php
@@ -6,117 +6,44 @@
         <script src="../assets/library/bootstrap.min.js"></script>
         <link rel="stylesheet" href="<?php echo base_url('assets/library/bootstrap.min.css')?>">
         <style>
-            .box-audio{
-                position: start;
-                margin-left: -38vmin;
-                margin-top: 2vmin;
+            span{
+                display:none;
             }
         </style>
     </head>
 <body>
  
 <div class="container">
+    <?php
+        $musicas = array(
+            'teste1' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'teste2' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'teste3' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'teste4' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'teste5' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'teste6' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+        )
+    ?>
 
-    <div class="panel panel-default col-sm-5 pull-left box-audio">
-        <div class="panel-heading">
-            <h3 class="panel-title">Principais Musicas Tocadas no Evento</h3>
-        </div>
-        <div class="panel-body">
-            <div class="panel-body text-center">
-                <div class="row col-md-12 col-xs-12 songs"><br>
-                    <p>Titulo</p>
-                    <audio controls
-                    data-info-album-art="https://farm9.staticflickr.com/8642/16106988340_058071cdbe_z.jpg"
-                    data-info-album-title="8874"
-                    data-info-artist="Iain Houston and Felix Gibbons"
-                    data-info-title="BeBop Aliens"
-                    data-info-label="Independent"
-                    data-info-year="2005"
-                    data-info-att="Music: Iain Houston and Felix Gibbons."
-                    data-info-att-link="https://github.com/iainhouston">
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.ogg" type="audio/ogg" />
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.mp3" type="audio/mpeg" />
-                    </audio>
-                </div>
-
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>
-                    <audio controls
-                    data-info-album-art="https://farm9.staticflickr.com/8642/16106988340_058071cdbe_z.jpg"
-                    data-info-album-title="8874"
-                    data-info-artist="Iain Houston and Felix Gibbons"
-                    data-info-title="BeBop Aliens"
-                    data-info-label="Independent"
-                    data-info-year="2005"
-                    data-info-att="Music: Iain Houston and Felix Gibbons."
-                    data-info-att-link="https://github.com/iainhouston">
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.ogg" type="audio/ogg" />
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.mp3" type="audio/mpeg" />
-                    </audio>
-                </div>
-
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>
-                    <audio controls
-                    data-info-album-art="https://farm9.staticflickr.com/8642/16106988340_058071cdbe_z.jpg"
-                    data-info-album-title="8874"
-                    data-info-artist="Iain Houston and Felix Gibbons"
-                    data-info-title="BeBop Aliens"
-                    data-info-label="Independent"
-                    data-info-year="2005"
-                    data-info-att="Music: Iain Houston and Felix Gibbons."
-                    data-info-att-link="https://github.com/iainhouston">
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.ogg" type="audio/ogg" />
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.mp3" type="audio/mpeg" />
-                    </audio>
-                </div>
-
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>
-                    <audio controls
-                    data-info-album-art="https://farm9.staticflickr.com/8642/16106988340_058071cdbe_z.jpg"
-                    data-info-album-title="8874"
-                    data-info-artist="Iain Houston and Felix Gibbons"
-                    data-info-title="BeBop Aliens"
-                    data-info-label="Independent"
-                    data-info-year="2005"
-                    data-info-att="Music: Iain Houston and Felix Gibbons."
-                    data-info-att-link="https://github.com/iainhouston">
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.ogg" type="audio/ogg" />
-                    <source src="http://playerdemo.iainhouston.com/tests/BeBopAliens.mp3" type="audio/mpeg" />
-                    </audio>
-                </div>
-            
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>
-                    <audio controls>
-                    <source src="http://www.w3schools.com/html/horse.ogg" type="audio/ogg" />
-                    <source src="http://www.w3schools.com/html/horse.mp3" type="audio/mpeg" />
-                    <a href="http://www.w3schools.com/html/horse.mp3">horse</a>
-                    An html5-capable browser is required to play this audio. 
-                    </audio>
-                </div>
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>   
-                    <audio controls>
-                    <source src="http://www.w3schools.com/html/horse.ogg" type="audio/ogg" />
-                    <source src="http://www.w3schools.com/html/horse.mp3" type="audio/mpeg" />
-                    <a href="http://www.w3schools.com/html/horse.mp3">horse</a>
-                    An html5-capable browser is required to play this audio. 
-                    </audio>
-                </div>
-                <div class="row col-md-12 col-xs-12"><br>
-                    <p>Titulo</p>
-                    <audio controls>
-                    <source src="http://www.w3schools.com/html/horse.ogg" type="audio/ogg" />
-                    <source src="http://www.w3schools.com/html/horse.mp3" type="audio/mpeg" />
-                    <a href="http://www.w3schools.com/html/horse.mp3">horse</a>
-                    An html5-capable browser is required to play this audio. 
-                    </audio>
-                </div>
-            </div>
-        </div>
-    </div>
- 
+    <ul class="list-group col-sm-4">
+        <?php foreach($musicas as $chave => $valor): ?>
+        <li class="list-group-item">
+            <p id="box-audio-title_<?php echo $chave?>"><?php echo $chave?></p>
+            <span class="box-audio_<?php echo $chave?>">
+                <audio id='4' controls>
+                    <source src="<?php echo $valor?>" type="audio/mpeg" /> 
+                </audio>
+            </span>
+        </li>
+        <script>
+        $(document).ready(function() {
+            $("#box-audio-title_<?php echo $chave?>").click(function() {
+                $(".box-audio_<?php echo $chave?>").toggle();
+            })
+        });
+        </script>
+        <?php endforeach ?>
+        <li class="list-group-item">Third item</li>
+    </ul>
 </body>
 </html>
