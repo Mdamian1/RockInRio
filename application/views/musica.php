f24717b509bc66bb9455084ed7aea4d05bc48c20
Dionathan de Córdova <dionathan_bass@hotmail.com>
2018-10-16 21:43:24 -0300

    Musicas novas

--- application/views/musica.php
+++ application/views/musica.php
@@ -16,19 +16,23 @@
 <div class="container">
     <?php
         $musicas = array(
-            'teste1' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
-            'teste2' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
-            'teste3' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
-            'teste4' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
-            'teste5' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
-            'teste6' => base_url('assets/musicas/Noah Guthrie - Whipping Post by The Allman Brothers.mp3'),
+            'All_Or_Nothing' => base_url('assets/musicas/01 - All Or Nothing.mp3'),
+            'Fight_Till_You_Die' => base_url('assets/musicas/02 - Fight Till You Die.mp3'),
+            'Greed' => base_url('assets/musicas/03 - Greed.mp3'),
+            'Knocked_Down' => base_url('assets/musicas/04 - Knocked Down.mp3'),
+            'catch_me_running_round' => base_url('assets/musicas/05 - no fun at all - catch me running round.mp3'),
+            'master_celebrator' => base_url('assets/musicas/06 - no fun at all - master celebrator.mp3'),
+            'Peaceful_Day' => base_url('assets/musicas/07 - Peaceful Day.mp3'),
+            'Time_Marches_On' => base_url('assets/musicas/08 - Time Marches On.mp3'),
+            'Celestial_Q&A' => base_url('assets/musicas/09 - Celestial Q&A.mp3'),
+            'Day_To_Daze' => base_url('assets/musicas/10 - Day To Daze.mp3'),
         )
     ?>
 
     <ul class="list-group col-sm-4">
         <?php foreach($musicas as $chave => $valor): ?>
         <li class="list-group-item">
-            <p id="box-audio-title_<?php echo $chave?>"><?php echo $chave?></p>
+            <p id="box-audio-title_<?php echo $chave?>"><?php echo str_replace('_',' ',$chave)?></p>
             <span class="box-audio_<?php echo $chave?>">
                 <audio id='4' controls>
                     <source src="<?php echo $valor?>" type="audio/mpeg" /> 
@@ -43,7 +47,6 @@
         });
         </script>
         <?php endforeach ?>
-        <li class="list-group-item">Third item</li>
     </ul>
 </body>
 </html>
