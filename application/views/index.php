ab2527c05e8836c424b3c48085630ae858be7578
Matheus Damian Pereira <matheusdamian70@gmail.com>
2018-11-11 21:53:53 -0200

    Alteração do Link pagina principal e Modal arrumado

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,8 @@
 
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
 
 <link rel="stylesheet" href="../assets/index/css/style.css">
+<link rel="stylesheet" href="<?php echo base_url('assets/library/bootstrap.min.css')?>">
 <article>
     <div class="centro">
         <div class="centroInterior">
@@ -11,12 +12,22 @@
             </div>
             <br><br>
             <div class="centroMenu">
-                <a class="centroOpcoes" href="<?php echo base_url('welcome/fotos')?>"><div class="centroOpcoes" id="fotos">FOTOS</div></a>
-                <a class="centroOpcoes" href="<?php echo base_url('welcome/videos')?>"><div class="centroOpcoes" id="video">VÍDEOS</div></a>
-                <a class="centroOpcoes" href="<?php echo base_url('welcome/musicas')?>"><div class="centroOpcoes" id="musica">MÚSICAS</div></a>
+                <a class="a" href="<?php echo base_url('welcome/fotos')?>">
+                    <div class="centroOpcoes" id="fotos">FOTOS</div>
+                </a>
+                <a class="a" href="<?php echo base_url('welcome/videos')?>">
+                    <div class="centroOpcoes" id="video">VÍDEOS</div>
+                </a>
+                <a class="a" href="<?php echo base_url('welcome/musicas')?>">
+                    <div class="centroOpcoes" id="musica">MÚSICAS</div>
+                </a>
             </div>
             <br><br>
         </div>
     </div>
     <script src="../assets/index/js/youtube.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </article>
